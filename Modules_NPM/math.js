8b218d66795bd157975adb2568da9705b410e735
Brandon Dalton <dabigin@gmail.com>
2023-04-26 14:53:10 -0500

    Section 32 329 Working With module.exports

---
+++ Modules_NPM/math.js
@@ -0,0 +1,21 @@
+const add = (x, y) => x + y;
+
+const PI = 3.14159;
+
+const square = (x) => x * x;
+
+exports.add = add;
+exports.square = square;
+exports.PI = PI;
+
+// const math = {
+//   add: add,
+//   PI: PI,
+//   square: square,
+// };
+
+//=========================
+// module.exports = math;
+// module.exports.add = add;
+// module.exports.PI = PI;
+// module.exports.square = square;
