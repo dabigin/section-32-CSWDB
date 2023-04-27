c30fc61ab6af838a34dbf9d5d5ceb161a0f625b9
Brandon Dalton <dabigin@gmail.com>
2023-04-27 14:12:10 -0500

    Section 32 Lecture 332 Installing Packages Jokes and Rainbow

--- Modules_NPM/Jokester/index.js
+++ Modules_NPM/Jokester/index.js
@@ -1,6 +1,7 @@
 const jokes = require("give-me-a-joke");
 const colors = require("colors");
 // To get a random dad joke
+//332 Installing Packages Jokes and Rainbow
 jokes.getRandomDadJoke(function (joke) {
   console.log(joke.rainbow);
 });
