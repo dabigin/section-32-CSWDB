3ac1c95a32ff4d370a19c42fcf45ea01f1cccb42
Brandon Dalton <dabigin@gmail.com>
2023-04-27 12:27:20 -0500

    Error on Section 32 Lecture 330 Requiring a directory

--- Modules_NPM/app.js
+++ Modules_NPM/app.js
@@ -1,4 +1,6 @@
-const math = require("./math");
-console.log(math.PI);
+// const math = require("./math");
+// console.log(math.PI);
 
-console.log(math.square(9));
+// console.log(math.square(9));
+const cats = require("./shelter");
+console.log("REQUIRED AN ENTIRE DIRECTORY!", cats);
